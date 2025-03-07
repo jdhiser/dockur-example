8e5c0bf940a25bb439aad344844498e6a1375971
Jason Hiser <jdhiser@gmail.com>
2025-03-07 07:28:57 -0500

    Better output + wait for windows to boot

--- restart-windows.sh
+++ restart-windows.sh
@@ -3,11 +3,27 @@
 
 main()
 {
+	# get the freshly installed windows
 	rsync win-storage-save win-storage
 	docker compose up
 
+
+	# Wait until SSH can connect
+	local tries=0
+	while ! nc -z -w 5 "localhost" "2022" && (( tries < 50 )) 
+	do
+		echo "Waiting for SSH to be available on $REMOTE_HOST:$REMOTE_PORT..."
+		sleep 5
+		((tries++))
+	done
+
+
+	# done
+	echo 
+	echo '****************************************'
 	echo 'Docker is up, connect with:'
 	echo 'sshpass -p gates ssh bill@gates -p 2022'
+	echo '****************************************'
 }
 
 main "$@"
