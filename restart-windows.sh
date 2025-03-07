5c4e7a457d126f097b83ceebdebbf64f7cbc9545
Jason Hiser <jdhiser@gmail.com>
2025-03-07 09:57:46 -0500

    Better i/o for user understanding, better detection of ssh being ready

--- restart-windows.sh
+++ restart-windows.sh
@@ -5,12 +5,14 @@ main()
 {
 	# get the freshly installed windows
 	rsync win-storage-save win-storage
-	docker compose up
+	docker compose up -d
 
+
+	ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:2022"
 
 	# Wait until SSH can connect
 	local tries=0
-	while ! nc -z -w 5 "localhost" "2022" && (( tries < 50 )) 
+	while ! sshpass -p gates ssh -o ConnectTimeout=5 -p 2022 bill@localhost exit 2> /dev/null  && (( tries < 50 )) 
 	do
 		echo "Waiting for SSH to be available on $REMOTE_HOST:$REMOTE_PORT..."
 		sleep 5
@@ -22,12 +24,12 @@ main()
 	echo 
 	echo '****************************************'
 	echo 'Docker is up, connect with:'
-	echo '	sshpass -p gates ssh bill@gates -p 2022'
 	echo
-	echo 'Press enter to shut down'
+	echo '	sshpass -p gates ssh -o StrictHostKeyChecking=off bill@localhost -p 2022'
+	echo
 	echo '****************************************'
+	read -p  'Press enter to shut down'
 
-	pause
 	docker compose down
 }
 
