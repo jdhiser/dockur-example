270fefd860abfc4e209fbe6f77fed3469c217aa7
Jason Hiser <jdhiser@gmail.com>
2025-03-07 07:25:25 -0500

    Better output

--- install-windows.sh
+++ install-windows.sh
@@ -5,8 +5,13 @@ main()
 {
 	docker compose up -d
 	sleep 30s
-	echo 'Starting firefox.  Close firefox when install appears complete in the GUI.'
-	firefox localhost:8006
+	echo
+	echo '****************************************************************************'
+	echo 'Starting firefox.  Close firefox when install appears complete in the GUI.  '
+	echo 'Make sure install.bat has finished running, it can take a while.'
+	echo '****************************************************************************'
+	echo
+	firefox localhost:8006 2> /dev/null
 	docker compose down
 	cp -r win-storage win-storage-save
 }
